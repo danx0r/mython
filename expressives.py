57900bb47674adc4f840052fff9e08b70f2c610b
danx0r <dan.miller@eye0.com>
2018-10-26 17:35:49 -0700

    experiments

--- expressives.py
+++ expressives.py
@@ -1,25 +1,57 @@
 class expressive(object):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, exp):
+        self.exp = exp
 
     def __eq__(self, b):
-        print (self, "==", b)
-        return {'eq': [self, b]}
+        # print (self, "==", b)
+        ret = expressive({'==': [self, b]})
+        # print (" RET:", ret)
+        return ret
+
+    def __ge__(self, b):
+        # print (self, "==", b)
+        ret = expressive({'>=': [self, b]})
+        # print (" RET:", ret)
+        return ret
+
+    def __lt__(self, b):
+        # print (self, "==", b)
+        ret = expressive({'<': [self, b]})
+        # print (" RET:", ret)
+        return ret
+
+    def __and__(self, b):
+        # print (self, "==", b)
+        ret = expressive({'&': [self, b]})
+        # print (" RET:", ret)
+        return ret
 
     def __repr__(self):
-        return ".%s" % self.name
+        if hasattr(self.exp, 'capitalize'):
+            return ".%s" % self.exp
+        else:
+            return "%s" % self.exp
 
 class expressive_maker(object):
 
-    def __getattr__(self, name):
-        # print ("getattr:", name)
-        return expressive(name)
+    def __getattr__(self, exp):
+        # print ("getattr:", exp)
+        return expressive(exp)
     
 _ = expressive_maker()
 
 if __name__=="__main__":
-    # b = _.bar
-    # print (b, b.name)
-    # print (_.bar == _.foo)
-    print (_.bar == (_.foo == _.baz))
+    from pprint import pprint
+    print ()
+    # bf = _.bar == _.foo
+    # print(bf)
+    # print ("==================")
+    # fz = _.foo == _.zap
+    # print(fz)
+    # print ("==================")
+    # bz = (bf == fz)
+    # print(bz)
+    # pprint (_.bar >= _.foo & _.baz < _.zap)
+    pprint (_.bar >= _.foo < _.zap)
+    pprint (_.bar >= (_.foo < _.zap))
---
+++ notes.py
@@ -0,0 +1,8 @@
+my.bar          #returns my where there is a bar key
+
+my.bar == 4
+
+my.bar < my.foo
+
+(my.bar & my.foo) | (my.xap in my.baz)
+
