55884ba3c4fe2a35d10aa0c6fb8c1face6084a54
danx0r <dan.miller@eye0.com>
2018-10-28 12:22:39 -0700

    parenthetic

--- parser.py
+++ parser.py
@@ -15,10 +15,18 @@ BINARIES = ['<', '>', '==', '<=', '>=', '!=', 'and', 'or']
 
 def _parse():
     ret = toks.pop(0)
-    if len(toks):
+    if ret == '(':
+        # print ("PARSE PAREN CLAUSE:", toks)
+        ret = _parse()
+        # print ("PARSED: %s REMAIN: %s", (ret,toks))
+        end = toks.pop(0)
+        if end != ')':
+            print ("SYNTAX ERROR: expected )", end, toks)
+            return 
+    if len(toks) and toks[0] != ')':            #so slick
         op = toks.pop(0)
         if op not in BINARIES:
-            print ("SYNTAX ERROR")
+            print ("SYNTAX ERROR: expected binary operator", op, toks)
             return
         ret = {op: [ret, _parse()]}
     return ret
@@ -29,5 +37,8 @@ if __name__=="__main__":
     s = ".bar == 4"
     print(parse(s))
     s = ".bar == 4  > 5"
+    print(parse(s))
     s = ".bar and .foo > 5"
+    print(parse(s))
+    s = "( a > b ) and ( c < d )"
     print(parse(s))
