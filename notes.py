15be2ac3e3b3650c3a8ed882863625ad65740f61
danx0r <dan.miller@eye0.com>
2018-10-28 11:45:11 -0700

    period full stop

--- notes.py
+++ notes.py
@@ -9,31 +9,31 @@ my=mython()
 my
 1> [{'bar': 4, 'foo': 5}, {'bar': 'three'}, {'foo': 6}]
 
-my["bar"]
+my[".bar"]
 1> [{'bar': 4, 'foo': 5}, {'bar': 'three'}]
 
-my["bar == 4"]
+my[".bar == 4"]
 1> [{'bar': 4, 'foo': 5}]
 
-my["foo"]
+my[".foo"]
 1> [{'bar': 4, 'foo': 5}, {'foo': 6}]
 
-my["bar < foo"]
+my[".bar < .foo"]         #how do we know 'foo' is a property, not a text string?
 1> [{'bar': 4, 'foo': 5}]
 
-my["!foo"]          #returns list where members do not have .foo
+my["!.foo"]          #returns list where members do not have .foo
 1> [{'bar': 'three'}]
 
-my["!foo && bar==three"]
+my["!.foo and bar=='three'"]
 1> [{'bar': 'three'}]
 
-my["!(foo == 6)"]
+my["!(.foo == 6)"]
 1> [{'bar': 4, 'foo': 5}, {'bar': 'three'}]
 
-#spaces:
-my["'foo bar' == 'Asperger Syndrome'"]
+#spaces (note period is _before_ quote mark):
+my[".'foo bar' == 'Asperger Syndrome'"]
 1> []
 
 #strings not numbers:
-my["foo == '3'"]
+my[".foo == '3'"]
 1> []
