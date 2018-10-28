ac926bed52d67f5bf64c0cbaa3d3db96d9e0a5ac
danx0r <dan.miller@eye0.com>
2018-10-28 16:32:40 -0700

    simplifico ad absurdum

--- mython.py
+++ mython.py
@@ -1,13 +1,4 @@
-from mydict import mydict
-
 class mython(list):
-    def __getattr__(self, item):
-        # print ("getattr..", item)
-        return self.handle_text_key(item)
-
-    # def __setattr__(self, key, value):
-    #     self[key] = value
-
     def __getitem__(self, item):
         # print ("getitem..", item)
         if type(item) == int:               #slices?
@@ -15,25 +6,31 @@ class mython(list):
         else:
             return self.handle_text_key(item)
 
-    def __eq__(self, b):
-        # print (self, "==", b)
-        return mython([x for x in self if x[self.__propertyname__]==b])
-
-    def handle_text_key(self, item):
-        ret = mython([x for x in self if isinstance(x, dict) and item in x])
-        ret.__propertyname__=item
+    def handle_text_key(self, exp):
+        print ("EXP:", exp)
+        while '.' in exp:
+            beg = exp.find('.')
+            if ' ' in exp[beg:]:
+                end = beg+exp[beg:].find(' ')
+            else:
+                end = len(exp)
+            key = exp[beg+1:end]
+            exp = exp[:beg]+'__x__["""'+key+'"""]'+exp[end:]
+        print ("handle_text:", exp)
+        ret = []
+        for __x__ in self:
+            try:
+                if eval(exp):
+                    ret.append(__x__)
+            except:
+                print ("~~~", __x__)
+                pass
         return ret
 
 if __name__=="__main__":
-    my=mython([1,{'foo':2},mydict(bar=1, foo=2, baz=3),{'bar':4}])
+    my=mython([{'foo':2}, dict(bar=1, foo=11, baz=3), {'bar':4}])
     print(my)
-    print(my[1])
-    print(my.foo)
-    print(my['bar'])
-    print ("==================")
-    print(my.bar == 4)
-    md=mydict()
-    md.bar=4
-    md.foo="bax"
-    my.insert(1,md)
-    print(my.bar == 4)
+    # print(my[".foo"])
+    # print(my["not .foo"])
+    # print(my[".foo == 2"])
+    print(my[".bar > 2 and .bar < 6 or .foo == 2"])
